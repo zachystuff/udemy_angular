d2389ce9c9e3db09573d7942e1d902b15de5ddca
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-10-28 11:30:02 -0500

    two way data binding using ngmodel! so cool

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created!";
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -19,7 +20,11 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
